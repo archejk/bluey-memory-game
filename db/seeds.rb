689afdd086241d85709910aae171b34624810613
archejk <jkarche13@gmail.com>
2024-11-03 13:44:35 +0800

    Setup Cloudinary Data: Populate Card Characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,17 @@ Card.create([
   { name: "Nana", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/nana_qs0jnq.png" },
   { name: "Jack", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/jack_ag0wwp.png" },
   { name: "Granny Rita", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545322/granny-rita_o8dpfl.png" },
-  { name: "Judo", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/judo_gjwni7.png" }
+  { name: "Judo", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/judo_gjwni7.png" },
+  { name: "Bently", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/bentley_jbz5kt.png" },
+  { name: "Chucky", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/chucky_gtltie.png" },
+  { name: "Buddy", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/buddy_rltult.png" },
+  { name: "Chloe", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/chloe_iwm6lb.png" },
+  { name: "Coco", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/coco_qunp6c.png" },
+  { name: "Dougie", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/dougie_mxwnw3.png" },
+  { name: "Dusty", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/dusty_zcqu7h.png" },
+  { name: "Fido", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/fido_sfbt3e.png" },
+  { name: "Doreen", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/doreen_tfffib.png" },
+  { name: "Digger", image_url: "https://res.cloudinary.com/dngtno2ra/image/upload/v1730545325/digger_u2nvbo.png" }
 ])
 
 # run the seed commands later if cloudinary images are uploaded to the cloud
